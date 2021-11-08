2faad00ea19cb6a9b0a8e0961f5f66b122c46910
Deven Shah <devenshah1974@gmail.com>
2021-11-08 08:35:22 +0000

    APR-2789 Added Data project and data context

---
+++ src/SFA.DAS.Charities.Data/Configuration/CharityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SFA.DAS.Charities.Domain.Entities;
+
+namespace SFA.DAS.Charities.Data.Configuration
+{
+    internal class CharityConfiguration : IEntityTypeConfiguration<Charity>
+    {
+        public void Configure(EntityTypeBuilder<Charity> builder)
+        {
+            builder.HasMany(c => c.Trustees).WithOne().HasPrincipalKey(x => x.Id).HasForeignKey(x => x.CharityId);
+        }
+    }
+}
