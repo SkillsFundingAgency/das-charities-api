0b46b08a78cc0c6e6c942c770ae3faac8305127b
Deven Shah <devenshah1974@gmail.com>
2021-11-12 16:33:48 +0000

    APR-2794 Implement download workflow

---
+++ src/SFA.DAS.Charities.Import.UnitTests/Functions/ImportCharityCommissionDataWorkflowTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SFA.DAS.Charities.Import.Functions.ImportCharityCommissionData;
+using SFA.DAS.Charities.Import.Functions.ImportCharityCommissionData.Activities;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SFA.DAS.Charities.Import.UnitTests.Functions
+{
+    public class ImportCharityCommissionDataWorkflowTests
+    {
+        [Fact]
+        public async Task ImportCharityCommissionData_ForEachFileInConfig_CallsDownloadActivity()
+        {
+            var configMock = new Mock<IConfiguration>();
+            configMock.SetupGet(x => x["CharityDownloadFileNames"]).Returns("file1, file2");
+
+            var subject = new ImportCharityCommissionDataWorkflow(configMock.Object);
+            var contextMock = new Mock<IDurableOrchestrationContext>();
+
+            await subject.ImportCharityCommissionData(contextMock.Object, Mock.Of<ILogger>());
+
+            contextMock.Verify(c => c.CallActivityAsync(nameof(ImportCharityCommissionDataActivity), "file1"));
+            contextMock.Verify(c => c.CallActivityAsync(nameof(ImportCharityCommissionDataActivity), "file2"));
+        }
+    }
+}
--- src/SFA.DAS.Charities.Import/Functions/ImportCharityCommissionData/ImportCharityCommissionDataWorkflow.cs
+++ src/SFA.DAS.Charities.Import/Functions/ImportCharityCommissionData/ImportCharityCommissionDataWorkflow.cs
@@ -1,16 +1,33 @@
 ﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.Charities.Import.Functions.ImportCharityCommissionData.Activities;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.Charities.Import.Functions.ImportCharityCommissionData
 {
     public class ImportCharityCommissionDataWorkflow
     {
-        [FunctionName(nameof(ImportCharityCommissionDataWorkflow))]
-        public static Task ImportCharityCommissionData([OrchestrationTrigger] IDurableOrchestrationContext context, ILogger logger)
+        private readonly IEnumerable<string> filenames; 
+
+        public ImportCharityCommissionDataWorkflow(IConfiguration configuration)
         {
-            return Task.CompletedTask;
+            filenames = configuration["CharityDownloadFileNames"].Split(',', System.StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        [FunctionName(nameof(ImportCharityCommissionDataWorkflow))]
+        public async Task ImportCharityCommissionData([OrchestrationTrigger] IDurableOrchestrationContext context, ILogger logger)
+        {
+            logger = context.CreateReplaySafeLogger(logger);
+            logger.LogDebug($"Downloading charity data files");
+
+            var tasks = filenames.Select(f => context.CallActivityAsync(nameof(ImportCharityCommissionDataActivity), f.Trim()));
+
+            await Task.WhenAll(tasks);
+            logger.LogDebug($"Finished Downloading charity data files");
         }
     }
 }
