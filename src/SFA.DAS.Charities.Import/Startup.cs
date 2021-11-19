593de1c299d8bb71aae01e79402bfe01a095bf4d
Deven Shah <devenshah1974@gmail.com>
2021-11-19 13:45:46 +0000

    APR-2833 Added nlogger

--- src/SFA.DAS.Charities.Import/Startup.cs
+++ src/SFA.DAS.Charities.Import/Startup.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using Polly;
 using Polly.Extensions.Http;
 using SFA.DAS.Charities.Import.Functions;
@@ -18,16 +20,35 @@ namespace SFA.DAS.Charities.Import.Functions
         public override void Configure(IFunctionsHostBuilder builder)
         {
             var configuration = BuildConfiguration(builder);
-            builder.Services.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), configuration));
+            
             RegisterCharityCommissionsHttpClient(builder, configuration);
 
             builder.Services.AddTransient<IDateTimeProvider, DateTimeProvider>();
         }
 
+        private static void AddNLog(IFunctionsHostBuilder builder)
+        {
+            var nLogConfiguration = new NLogConfiguration();
+
+            builder.Services.AddLogging((options) =>
+            {
+                options.AddFilter(typeof(Startup).Namespace, LogLevel.Information);
+                options.SetMinimumLevel(LogLevel.Trace);
+                options.AddNLog(new NLogProviderOptions
+                {
+                    CaptureMessageTemplates = true,
+                    CaptureMessageProperties = true
+                });
+                options.AddConsole();
+
+                nLogConfiguration.ConfigureNLog();
+            });
+        }
+
         private static IConfiguration BuildConfiguration(IFunctionsHostBuilder builder)
         {
             var configuration = builder.GetContext().Configuration;
-            return new ConfigurationBuilder()
+            var config = new ConfigurationBuilder()
                 .AddConfiguration(configuration)
                 .AddAzureTableStorage(options => 
                 {
@@ -37,6 +58,8 @@ namespace SFA.DAS.Charities.Import.Functions
                     options.PreFixConfigurationKeys = false;
                 })
                 .Build();
+            builder.Services.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), config));
+            return config;
         }
 
         private static void RegisterCharityCommissionsHttpClient(IFunctionsHostBuilder builder, IConfiguration configuration)
