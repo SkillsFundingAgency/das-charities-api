c2810f327eb2497165a7e1cbb237d57bd53eceaa
Deven Shah <devenshah1974@gmail.com>
2021-11-19 15:41:19 +0000

    APR-2795 Add service to extract data from zip file (#12)

---
+++ src/SFA.DAS.Charities.Import/Infrastructure/CharityCommissionDataExtractService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Charities.Import.Infrastructure
+{
+    public class CharityCommissionDataExtractService : ICharityCommissionDataExtractService
+    {
+        private readonly ILogger<CharityCommissionDataExtractService> _logger;
+
+        public CharityCommissionDataExtractService(ILogger<CharityCommissionDataExtractService> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<List<T>> ExtractData<T>(Stream zipFile)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            using var archive = new ZipArchive(zipFile, ZipArchiveMode.Read);
+            var zipEntry = archive.Entries.FirstOrDefault();
+
+            try
+            {
+                var reader = new StreamReader(zipEntry.Open());
+                var json = await reader.ReadToEndAsync();
+                var result = JsonConvert.DeserializeObject<List<T>>(json);
+                _logger.LogInformation($"It took {stopWatch.ElapsedMilliseconds}ms to extract {zipEntry.Name}");
+
+                return result;
+            }
+            catch (JsonReaderException ex)
+            {
+                _logger.LogError(ex, $"An error occurred trying to extract data {zipEntry.Name}");
+                throw;
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.Charities.Import/Infrastructure/ICharityCommissionDataExtractService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Charities.Import.Infrastructure
+{
+    internal interface ICharityCommissionDataExtractService
+    {
+        Task<List<T>> ExtractData<T>(Stream zipFile);
+    }
+}
