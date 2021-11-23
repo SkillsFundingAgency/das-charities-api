65fa4b3492bb50e76a0454679bc149cc3510fc23
Deven Shah <devenshah1974@gmail.com>
2021-11-23 15:46:16 +0000

    APR-2805 Added trustee staging repository

---
+++ src/SFA.DAS.Charities.Data/Repositories/CharityTrusteeStagingRepository.cs
@@ -0,0 +1,26 @@
+﻿using EFCore.BulkExtensions;
+using SFA.DAS.Charities.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Charities.Data.Repositories
+{
+    public class CharityTrusteeStagingRepository : ICharityTrusteeStagingRepository
+    {
+        private readonly CharitiesDataContext _charitiesDataContext;
+
+        public CharityTrusteeStagingRepository(CharitiesDataContext charitiesDataContext)
+        {
+            _charitiesDataContext = charitiesDataContext;
+        }
+
+        public async Task BulkInsert(List<CharityTrusteeStaging> data)
+        {
+            using var tx = await _charitiesDataContext.Database.BeginTransactionAsync();
+
+            await _charitiesDataContext.BulkInsertAsync(data);
+
+            await tx.CommitAsync();
+        }
+    }
+}
---
+++ src/SFA.DAS.Charities.Data/Repositories/ICharityTrusteeStagingRepository.cs
@@ -0,0 +1,11 @@
+﻿using SFA.DAS.Charities.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Charities.Data.Repositories
+{
+    public interface ICharityTrusteeStagingRepository
+    {
+        Task BulkInsert(List<CharityTrusteeStaging> data);
+    }
+}
