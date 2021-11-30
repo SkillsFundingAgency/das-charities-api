4b0598b2071ff5f62ce85b65f897c578f0e8b663
Deven Shah <devenshah1974@gmail.com>
2021-11-30 21:03:32 +0000

    APR-2799 Added API endpoint to retrieve specific charity details

---
+++ src/SFA.DAS.Charities.Api/Controllers/CharitiesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Charities.Data.Repositories;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Charities.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharitiesController : ControllerBase
+    {
+        private readonly ICharitiesReadRepository _charityReadRepository;
+        private readonly ILogger<CharitiesController> _logger;
+
+        public CharitiesController(ICharitiesReadRepository charityReadRepository, ILogger<CharitiesController> logger)
+        {
+            _charityReadRepository = charityReadRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{registrationNumber}")]
+        public async Task<IActionResult> GetCharityDetails(int registrationNumber)
+        {
+            var charity = await _charityReadRepository.GetCharityById(registrationNumber);
+
+            if (charity == null)
+            {
+                _logger.LogInformation("Charity with registration number: {registrationNumber} not found", registrationNumber);
+                return new NotFoundObjectResult($"Charity with registration number: {registrationNumber} not found");
+            }
+
+            return new OkObjectResult(charity);
+        }
+    }
+}
---
+++ src/SFA.DAS.Charities.Data/Repositories/CharitiesReadRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Charities.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Charities.Data.Repositories
+{
+    public class CharitiesReadRepository : ICharitiesReadRepository
+    {
+        private readonly CharitiesDataContext _charitiesDataContext;
+
+        public CharitiesReadRepository(CharitiesDataContext charitiesDataContext)
+        {
+            _charitiesDataContext = charitiesDataContext;
+        }
+        public Task<Charity> GetCharityById(int registrationNumber)
+        {
+            return _charitiesDataContext.Charities
+                .Include(c => c.Trustees)
+                .FirstOrDefaultAsync(c => c.RegistrationNumber == registrationNumber && c.LinkedCharityId == 0);
+        }
+    }
+}
